6ebc96bf83a912da0b55f9df1eea039c1f7270d7
Alireza <alirezaboloorian@gmail.com>
2024-04-23 20:00:40 -0700

    Added multiple asteroids

--- Galaga.sv
+++ Galaga.sv
@@ -3,14 +3,13 @@
 module vga(input  logic clk, reset,
            input  logic keyright, keyleft, keyup, keydown, 
            input  logic [9:0] seed,
-			  output logic vcc,
            output logic vgaclk,          // 25.175 MHz VGA clock 
            output logic hsync, vsync, 
            output logic sync_b, blank_b, // to monitor & DAC 
            output logic [7:0] r, g, b);  // to video DAC 
 
   logic [9:0] x, y;
- 
+	
   // divide 50 MHz input clock by 2 to get 25 MHz clock
   always_ff @(posedge clk, posedge reset)
     if (reset)
@@ -75,12 +74,31 @@ endmodule
 module videoGen(input logic [9:0] x, y, seed,
 					input logic keyright, keyleft, keyup, keydown, vsync, reset,
 					output logic [7:0] r, g, b); 
-  logic rpixel, apixel;
+  logic rpixel, a1pixel, a2pixel, a3pixel, a4pixel, a5pixel, a6pixel, spawnclk;
+  logic [5:0] en;
+  logic [9:0] RNGPos;
+
+   lfsr lfsr1(vsync, reset, seed, RNGPos);
+   clock clock1(reset, vsync, spawnclk);
+   asteoridGenerator asteoridGenerator1(reset, spawnclk, en);
 
   rocket r1(x, y, vsync, reset, keyright, keyleft, keyup, keydown, rpixel);
-  asteroid a1(x, y, seed, reset, vsync, apixel);
+  asteroid a1(x, y, RNGPos, reset, vsync, en[0], a1pixel);
+  asteroid a2(x, y, RNGPos, reset, vsync, en[1], a2pixel);
+  asteroid a3(x, y, RNGPos, reset, vsync, en[2], a3pixel);
+  asteroid a4(x, y, RNGPos, reset, vsync, en[3], a4pixel);
+  asteroid a5(x, y, RNGPos, reset, vsync, en[4], a5pixel);
+  asteroid a6(x, y, RNGPos, reset, vsync, en[5], a6pixel);
+
   
-  assign {r, g, b} = rpixel ? 24'hFFFFFF : apixel ? 24'h8968CD : 24'h000000; 
+  always_comb begin
+   if (rpixel) 
+			{r, g, b} = 24'hFFFFFF;
+   else if ((a1pixel)|(a2pixel)|(a3pixel)|(a4pixel)|(a5pixel)|(a6pixel)) 
+			{r, g, b} = 24'h8968CD;
+   else
+			{r, g, b} = 24'h000000;
+	end
 
 endmodule
 
@@ -137,7 +155,6 @@ module rocket(input logic [9:0] x, y,
 			verticalMove <= verticalMove;
 	end
 	
-  
 	 assign xleft = 10'd312;
 	 assign xright = 10'd326;
 	 assign ytop = 10'd452;
@@ -156,8 +173,8 @@ module rocket(input logic [9:0] x, y,
     end 
 endmodule
 	 
-module asteroid(input logic [9:0] x, y, seed,
-						input logic reset, vsync, 
+module asteroid(input logic [9:0] x, y, RNGPos,
+						input logic reset, vsync, en, 
                 output logic apixel); 
 
 // Data Structure for Asteroid Shape
@@ -184,32 +201,37 @@ logic [19:0][29:0] asteroid_shape = {
 	  30'b000000000000000111111000000000
 };
 	
-	 logic [9:0] xleft, xright, ytop, ybottom, RNGpos, moveDown;
-	 logic numbercatch;
-
-	 catch c1(vsync, reset, moveDown, numbercatch);
+	 logic [9:0] xleft, xright, ytop, ybottom, moveDown, initialP;
 	 
      // Asteroid moving down
 	always_ff @(posedge vsync, posedge reset) begin
-	if (reset)
+	if (reset) begin
 		moveDown <= 0;
-    else if (moveDown == 10'd480)
+		initialP <= RNGPos;
+	end
+	else if (~en) begin
+		moveDown <= 0;
+		initialP <= RNGPos;
+		end
+    else if (moveDown >= 10'd480) begin
         moveDown <= 0;
-	else
+		initialP <= RNGPos;
+	end
+	else begin
 		moveDown <= moveDown + 10'd1;
+		initialP <= initialP;
     end
+	end
 
-	 lfsr lfsr1(vsync, reset, numbercatch, seed, RNGpos);
-	 
-	 assign xleft = RNGpos;
-	 assign xright = RNGpos + 10'd30;
+	 assign xleft = initialP;
+	 assign xright = initialP + 10'd30;
 	 assign ytop = 10'd0;
 	 assign ybottom = 10'd20;
 
 
   // Inside asteroid module, assuming asteroid starts at X=310, Y=0
   always_comb begin
-	if ((x >= xleft) && (x < xright) &&
+	if ((en) && (x >= xleft) && (x < xright) &&
         (y-moveDown+10'd470 >= ytop+10'd470) && (y-moveDown+10'd470 < ybottom+10'd470) &&
         (asteroid_shape[y-ytop-moveDown][x-xleft])) begin
 	  apixel = 1; 
@@ -220,40 +242,54 @@ logic [19:0][29:0] asteroid_shape = {
   end
 endmodule
 
-module lfsr (input logic clk, reset,
-				input logic number_catch,
+module lfsr(input logic clk, reset,
 				input logic [9:0] seed,
-				output logic [9:0] RNG);
-				
+				output logic [9:0] RNG);		
 	logic [9:0] count;
 	
 	always_ff @(posedge clk, posedge reset) begin
-		if (reset)				count <= seed;
+		if (reset)	begin		count <= seed;
+		end
 		else 					count <= {count[0] ^ count[9], count[9:1]};
 	end 
-		
-	always_ff @(posedge number_catch, posedge reset) begin
-		if (reset) 				RNG <= 10'd0;
-		else 				    RNG <= count % 10'd641;
-	end
-	
+
+	assign RNG = count % 10'd641;
+
 endmodule
 
-module catch (input logic vsync, reset,
-					input logic [9:0] moveDown,
-					output logic numbercatch);
-	logic count;
-	
-	always_ff @(posedge vsync, posedge reset) begin
-		if (reset)
-				count <= 1'b1;
-       else if (moveDown == 10'd479) 
-            count <= 1'b1;
-        else
-            count <= 1'b0;
-    end
-	 
-assign numbercatch = count;
-	 
+module asteoridGenerator(input logic reset, clk, 
+						output logic [5:0] en);
+	logic [5:0] count;
+
+always_ff @(posedge clk, posedge reset) begin
+	if (reset) begin
+		count <= 6'b000000;
+	end
+	else if (count == 6'b111111) begin
+		count <= count;
+	end
+	else begin
+		count <= (count << 1) + 6'b000001;
+	end
+	end
+
+	assign en = count;
+
 endmodule
-					
+
+module clock(input logic reset, vsync,
+				output logic clk);
+	logic [7:0] count;
+
+	always_ff @(posedge reset, posedge vsync) begin
+		if (reset) begin
+			count <= 8'd0;
+		end
+		else begin
+			count <= count + 8'd1;
+		end
+	end
+
+	assign clk = count[7];
+
+endmodule
